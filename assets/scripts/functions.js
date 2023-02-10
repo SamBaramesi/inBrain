e3f4e40b6873855ef9449191f3ac28a50812ee51
SamBaramesi <200750@student.glu.nl>
2023-02-10 15:32:43 +0100

    Update

---
+++ assets/scripts/functions.js
@@ -0,0 +1,46 @@
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less";
+        moreText.style.display = "inline";
+    }
+}
+
+function readMoreDiv() {
+    var btn = document.getElementById("showMoreBtn");
+    var showMore = document.getElementById("showMore");
+
+    if (showMore.style.display === "none") {
+        showMore.style.display = "inline";
+        btn.innerHTML = "Minder Zien";
+    } else {
+        showMore.style.display = "none";
+        btn.innerHTML = "Meer Zien";
+    }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
