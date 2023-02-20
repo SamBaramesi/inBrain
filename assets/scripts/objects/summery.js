d8e0a750638cdaff4bfcde336f8b4098b2bb1b5d
SamBaramesi <200750@student.glu.nl>
2023-02-20 15:59:36 +0100

    Start JSON-HTML connection

---
+++ assets/scripts/objects/summery.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $.getJSON("facature.json", function (data) {
+
+        let column1 = data[2].sectionContent[0].sectionContent[0].columnContent
+        column1.forEach(row => {
+            document.getElementById('colOneJSON').innerHTML += `<li><span class="fa-li"><i class="${row.objectClass}"></i></span>${row.objectText}</li>`;
+        });
+    });
+});
+
+$(document).ready(function(){
+    $.getJSON("facature.json", function (data) {
+
+        let column2 = data[2].sectionContent[0].sectionContent[1].columnContent
+        console.log(column2)
+
+        column2.forEach(row => {
+            
+            document.getElementById('colTwoJSON').innerHTML += `<li><span class="fa-li"><i class="${row.objectClass}"></i></span>${row.objectText}</li>`;
+        
+        });
+        
+    });
+});
