c171dd932d4d8b740be5961967da6d0d3597c463
SamBaramesi <200750@student.glu.nl>
2023-02-27 15:32:37 +0100

    Calendar Update + small adjustments

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -2,7 +2,7 @@ function readMore() {
     let moreText = document.getElementById("more");
     let btnText = document.getElementById("myBtn");
 
-        if (moreText.style.display == "none"){
+        if (moreText.style.display === "none" || !moreText.style.display){
             moreText.style.display = "inline";
             btnText.innerHTML = "Read Less";
         } else {
@@ -16,7 +16,7 @@ function readMoreDiv() {
     var btn = document.getElementById("showMoreBtn");
     var showMore = document.getElementById("showMore");
 
-    if (showMore.style.display === "none") {
+    if (showMore.style.display === "none" || !showMore.style.display) {
         showMore.style.display = "inline";
         btn.innerHTML = "Minder Zien";
     } else {
